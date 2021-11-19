cf65e33461052122681b6255df9f2572bde58c68
Jack-Warne <92381475+Jack-Warne@users.noreply.github.com>
2021-11-19 10:59:08 +0000

    Update and rename Chapter 1 exercise 4 simple sums.cpp to Chapter 1 exercise 4 simple sums/ whole number value so an integer Completed.cpp

---
+++ Chapter 1 exercise 4 simple sums/ whole number value so an integer Completed.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	// setting the variables 
+	int no1 = 8; // whole number value so an integer
+	int no2 = 10;  // whole number value so an integer
+	int total;  // whole number value so an integer
+
+	total = no1 + no2; // calculates the total
+
+	// outputting the total
+	cout << total << endl;
+
+}
